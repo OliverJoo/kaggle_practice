c64e26584240e8c6fbf5193331e81dca78f37cea
OliverJoo <jjaemoon@nate.com>
2023-02-20 16:53:59 +0900

    modify code for pipeline

--- MDL/titanicML_New.py
+++ MDL/titanicML_New.py
@@ -1,16 +1,14 @@
 import numpy as np
 from sklearn.compose import ColumnTransformer
-from sklearn.datasets import fetch_openml
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
 from sklearn.linear_model import LogisticRegression
 import lightgbm as lgb
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 import pandas as pd
 
 # to see all data
@@ -19,38 +17,46 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 200)
 pd.set_option('display.colheader_justify', 'left')
 
-np.random.seed(0)
+titanic_df = pd.read_csv('./Data/train.csv')
+X_test = pd.read_csv('./Data/test.csv')
+y_test = pd.read_csv('./Data/gender_submission.csv')
+X_train = titanic_df.drop('Survived', axis=1)
+y_train = titanic_df['Survived']
 
-X, y = fetch_openml('titanic', version=1, as_frame=True, return_X_y=True)
-
-number_features_median = ['age']
 number_tranformer_median = Pipeline(steps=[('imputer_median', SimpleImputer(strategy='median')), ('scaler', StandardScaler())])
-
-number_features_constant = ['fare']
 number_tranformer_constant = Pipeline(steps=[('imputer_constant', SimpleImputer(strategy='constant', fill_value=0)), ('scaler', StandardScaler())])
+categorical_tranformer = OneHotEncoder(handle_unknown='ignore', sparse=False)
+# categorical_tranformer = OrdinalEncoder()
 
-categorical_features = ['embarked', 'sex', 'pclass']
-categorical_tranformer = OneHotEncoder(handle_unknown='ignore')
+preprocessor = ColumnTransformer(transformers=[('numb_median', number_tranformer_median, ['Age']),
+                                               ('numb_constant', number_tranformer_constant, ['Fare']),
+                                               ('drop_out', 'drop', ['PassengerId', 'Name', 'Ticket']),
+                                               ('category', categorical_tranformer, ['Embarked', 'Sex', 'Pclass'])])
 
-preprocessor = ColumnTransformer(transformers=[('number_median', number_tranformer_median, number_features_median),
-                                               ('number_constant', number_tranformer_constant, number_features_constant),
-                                               ('category', categorical_tranformer, categorical_features)])
-
-lr_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', LogisticRegression(solver='liblinear', warm_start=True, tol=0.01, random_state=20))])
+lr_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier',LogisticRegression(solver='liblinear', warm_start=True, tol=0.01, random_state=20))])
 dt_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', DecisionTreeClassifier(random_state=20))])
 rf_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', RandomForestClassifier(random_state=20, n_estimators=300, max_depth=23))])
 lgb_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', lgb.LGBMClassifier(random_state=20, n_estimators=300))])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=20)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=20)
+
 
 lr_clf.fit(X_train, y_train)
-print(f'model score: {lr_clf.score(X_test, y_test)}')
+lr_pred = lr_clf.predict(X_test)
+print(f'LogisticRegression model score: {lr_clf.score(X_train, y_train):.4f}')
+# print(f'LogisticRegression MAE : {mean_absolute_error(y_test["Survived"], lr_pred):.4f}')
 
 dt_clf.fit(X_train, y_train)
-print(f'model score: {dt_clf.score(X_test, y_test)}')
+dt_pred = dt_clf.predict(X_test)
+print(f'DecisionTreeClassifier model score: {dt_clf.score(X_train, y_train):.4f}')
+# print(f'LogisticRegression MAE : {mean_absolute_error(y_test["Survived"], dt_pred):.4f}')
 
 rf_clf.fit(X_train, y_train)
-print(f'model score: {rf_clf.score(X_test, y_test)}')
+rf_pred = rf_clf.predict(X_test)
+print(f'RandomForestClassifier model score: {rf_clf.score(X_train, y_train):.4f}')
+# print(f'LogisticRegression MAE : {mean_absolute_error(y_test["Survived"], rf_pred):.4f}')
 
 lgb_clf.fit(X_train, y_train)
-print(f'model score: {lgb_clf.score(X_test, y_test)}')
+lgb_pred = lgb_clf.predict(X_test)
+print(f'LGBMClassifier model score: {lgb_clf.score(X_train, y_train):.4f}')
+# print(f'LogisticRegression MAE : {mean_absolute_error(y_test["Survived"], lgb_pred):.4f}')
