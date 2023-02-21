51250ae40bc83fe1d634249fff660adde0864c10
OliverJoo <jjaemoon@nate.com>
2023-02-22 01:10:36 +0900

    test cross validation

--- MDL/titanicML_New.py
+++ MDL/titanicML_New.py
@@ -9,36 +9,34 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
 from sklearn.model_selection import cross_val_score
 
-# to see all data
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.width', 200)
-pd.set_option('display.colheader_justify', 'left')
-
-titanic_df = pd.read_csv('./Data/train.csv')
-X_test = pd.read_csv('./Data/test.csv')
-y_test = pd.read_csv('./Data/gender_submission.csv')
+# read dataset
+titanic_df = pd.read_csv('./Data/train.csv')  #  to train
+X_test = pd.read_csv('./Data/test.csv')  # to test
+y_test = pd.read_csv('./Data/gender_submission.csv')  # to validation
 X_train = titanic_df.drop('Survived', axis=1)
 y_train = titanic_df['Survived']
 
-number_tranformer_median = Pipeline(
-    steps=[('imputer_median', SimpleImputer(strategy='median')), ('scaler', StandardScaler())])
-number_tranformer_constant = Pipeline(
-    steps=[('imputer_constant', SimpleImputer(strategy='constant', fill_value=0)), ('scaler', StandardScaler())])
+# define Pipeline for numeric transformer for imputation
+number_tranformer_median = Pipeline(steps=[('imputer_median', SimpleImputer(strategy='median')), ('scaler', StandardScaler())])
+number_tranformer_constant = Pipeline(steps=[('imputer_constant', SimpleImputer(strategy='constant', fill_value=0)), ('scaler', StandardScaler())])
+
+# define Piple for categorical transformer by OneHoeEncoder and OridnalEncoder
 categorical_tranformer = OneHotEncoder(handle_unknown='ignore', sparse=False)
 # categorical_tranformer = OrdinalEncoder()
 
+# set preprocessor
 preprocessor = ColumnTransformer(transformers=[('numb_median', number_tranformer_median, ['Age']),
                                                ('numb_constant', number_tranformer_constant, ['Fare']),
                                                ('drop_out', 'drop', ['PassengerId', 'Name', 'Ticket']),
                                                ('category', categorical_tranformer, ['Embarked', 'Sex', 'Pclass'])])
 
+# set MDLing Pipleline
 lr_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', LogisticRegression(solver='liblinear', warm_start=True, tol=0.01, random_state=20))])
 dt_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', DecisionTreeClassifier(random_state=20))])
-
 rf_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', RandomForestClassifier(random_state=20, n_estimators=300, max_depth=23))])
 lgb_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', lgb.LGBMClassifier(random_state=20, n_estimators=300))])
 
@@ -46,7 +44,12 @@ lgb_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', lgb.LGBMClass
 
 
 def get_score(n_estimators):
-    my_pipeline = Pipeline(steps=[('preprocessor', preprocessor), ('classifier', RandomForestClassifier(random_state=20, n_estimators=n_estimators, max_depth=23))])
+    """
+    temp test function: get cross-validation MAE score(not for titanicML, this will be removed)
+    :param n_estimators:
+    :return: mean of 5 cross validation scores
+    """
+    my_pipeline = Pipeline(steps=[('preprocessor', preprocessor), ('classifier', RandomForestRegressor(random_state=20, n_estimators=n_estimators))])
     scores = -1 * cross_val_score(my_pipeline, X_train, y_train, cv=5, scoring='neg_mean_absolute_error')
 
     return scores.mean()
@@ -58,12 +61,9 @@ def get_score(n_estimators):
 # n_estimators_best = min(scores, key=scores.get)
 # print(n_estimators_best)
 
-
-
 lr_clf.fit(X_train, y_train)
 lr_pred = lr_clf.predict(X_test)
 print(f'LogisticRegression model score: {lr_clf.score(X_train, y_train):.4f}')
-scores = -1 * cross_val_score(lr_clf, X_train, y_train, cv=5, scoring='neg_mean_absolute_error')
 # print(f'LogisticRegression MAE : {mean_absolute_error(y_test["Survived"], lr_pred):.4f}')
 
 dt_clf.fit(X_train, y_train)
