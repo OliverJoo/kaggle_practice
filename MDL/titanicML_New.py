f8469ec4315000f0bd500a6eea913fb65be878c6
OliverJoo <jjaemoon@nate.com>
2023-02-21 23:44:00 +0900

    cross-validation

--- MDL/titanicML_New.py
+++ MDL/titanicML_New.py
@@ -10,6 +10,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 import pandas as pd
+from sklearn.model_selection import cross_val_score
 
 # to see all data
 pd.set_option('display.max_columns', None)
@@ -23,8 +24,10 @@ y_test = pd.read_csv('./Data/gender_submission.csv')
 X_train = titanic_df.drop('Survived', axis=1)
 y_train = titanic_df['Survived']
 
-number_tranformer_median = Pipeline(steps=[('imputer_median', SimpleImputer(strategy='median')), ('scaler', StandardScaler())])
-number_tranformer_constant = Pipeline(steps=[('imputer_constant', SimpleImputer(strategy='constant', fill_value=0)), ('scaler', StandardScaler())])
+number_tranformer_median = Pipeline(
+    steps=[('imputer_median', SimpleImputer(strategy='median')), ('scaler', StandardScaler())])
+number_tranformer_constant = Pipeline(
+    steps=[('imputer_constant', SimpleImputer(strategy='constant', fill_value=0)), ('scaler', StandardScaler())])
 categorical_tranformer = OneHotEncoder(handle_unknown='ignore', sparse=False)
 # categorical_tranformer = OrdinalEncoder()
 
@@ -33,17 +36,34 @@ preprocessor = ColumnTransformer(transformers=[('numb_median', number_tranformer
                                                ('drop_out', 'drop', ['PassengerId', 'Name', 'Ticket']),
                                                ('category', categorical_tranformer, ['Embarked', 'Sex', 'Pclass'])])
 
-lr_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier',LogisticRegression(solver='liblinear', warm_start=True, tol=0.01, random_state=20))])
+lr_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', LogisticRegression(solver='liblinear', warm_start=True, tol=0.01, random_state=20))])
 dt_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', DecisionTreeClassifier(random_state=20))])
+
 rf_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', RandomForestClassifier(random_state=20, n_estimators=300, max_depth=23))])
 lgb_clf = Pipeline(steps=[('prepro', preprocessor), ('classifier', lgb.LGBMClassifier(random_state=20, n_estimators=300))])
 
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=20)
 
 
+def get_score(n_estimators):
+    my_pipeline = Pipeline(steps=[('preprocessor', preprocessor), ('classifier', RandomForestClassifier(random_state=20, n_estimators=n_estimators, max_depth=23))])
+    scores = -1 * cross_val_score(my_pipeline, X_train, y_train, cv=5, scoring='neg_mean_absolute_error')
+
+    return scores.mean()
+
+# scores = {}
+# for i in range(1, 10):
+#     scores[50*i] = get_score(50*i)
+#
+# n_estimators_best = min(scores, key=scores.get)
+# print(n_estimators_best)
+
+
+
 lr_clf.fit(X_train, y_train)
 lr_pred = lr_clf.predict(X_test)
 print(f'LogisticRegression model score: {lr_clf.score(X_train, y_train):.4f}')
+scores = -1 * cross_val_score(lr_clf, X_train, y_train, cv=5, scoring='neg_mean_absolute_error')
 # print(f'LogisticRegression MAE : {mean_absolute_error(y_test["Survived"], lr_pred):.4f}')
 
 dt_clf.fit(X_train, y_train)
